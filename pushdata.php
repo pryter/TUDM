7888167625d4b81268a60dd305a357e6112d829f
Peter <peterphongpak@gmail.com>
2020-09-21 01:07:59 +0700

    Bug patched

--- pushdata.php
+++ pushdata.php
@@ -8,14 +8,21 @@ if(isset($_POST["time"]) && isset($_SESSION["user"]))
     $obj_ref = $dbs->read_obj();
     $ref = array_count_values($obj_ref);
     if($ref[$_POST["time"]] < 6 || !isset($ref[$_POST["time"]])){
-        $res = $dbs->append($_SESSION["user"],$_POST["time"]);
-        if($res){
-            $db2 = new database("database/users.dbs");
-            setcookie("QUE",$_POST["time"],time() + (10 * 365 * 24 * 60 * 60));
-            setcookie("USER",$_SESSION["user"],time() + (10 * 365 * 24 * 60 * 60));
-            $db2->eliminate($_SESSION["user"]);
+        if(!array_key_exists($_SESSION["user"],$obj_ref)) {
+            $res = $dbs->append($_SESSION["user"], $_POST["time"]);
+            if ($res) {
+                $db2 = new database("database/users.dbs");
+                setcookie("QUE", $_POST["time"], time() + (10 * 365 * 24 * 60 * 60));
+                setcookie("USER", $_SESSION["user"], time() + (10 * 365 * 24 * 60 * 60));
+                $db2->eliminate($_SESSION["user"]);
+                session_destroy();
+                $status = "success";
+            }
+        }else{
+            setcookie("QUE", "", time() - 3600);
+            setcookie("USER", "", time() - 3600);
             session_destroy();
-            $status = "success";
+            header("Location: index.php");
         }
     }
     echo json_encode(array("re" => $status));
--- reset.php
+++ reset.php
@@ -2,6 +2,7 @@
 session_start();
 if(isset($_SESSION["user"]) && $_SESSION["user"] == "admin") {
     setcookie("QUE", "", time() - 3600);
+    setcookie("USER", "", time() - 3600);
     session_destroy();
 }
 header("Location: index.php");
